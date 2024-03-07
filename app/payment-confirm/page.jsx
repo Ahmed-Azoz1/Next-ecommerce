0326f33d8d61b0b2637ed1748a89779f88644c1d
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-03-07 11:08:42 +0200

    error payment

--- app/payment-confirm/page.jsx
+++ app/payment-confirm/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import Favicon from '/public/logo-1.png';
+
 
 export const metadata = {
     title: "Product",
