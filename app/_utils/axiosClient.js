0c48a60fa8db783ee0460b404a8b8a2553c2c8b5
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-03-07 06:26:36 +0200

    axios api

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 const {default:axios} = require('axios') // import axios
 
 const apikey=process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl='http://localhost:1337/api';
+const apiUrl='https://strapi-api-app-h58t.onrender.com/api';
 
 
 const axiosClient = axios.create({
